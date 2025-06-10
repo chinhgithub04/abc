10d15fe6288ec3bbdf1d5f7113c305f1fd83a7d8
DKhoa04 <duykhoa12012004@gmail.com>
2025-06-10 11:48:22 +0700

    add ctsv

---
+++ Scholarship_Distribution_Management_System/Areas/CTSV/Controllers/TrangChuController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Scholarship_Distribution_Management_System.Areas.CTSV.Controllers
+{
+    public class TrangChuController : Controller
+    {
+        [Area("CTSV")]
+        [Route("CTSV/[controller]/[action]")]
+        [Authorize(Roles = "CTSV")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
